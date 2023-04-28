afdff827754f754703bc32724d9580d1e5dbbaa9
choisaelim <choisaelim@gmail.com>
2023-04-28 23:14:44 +0900

    로그인 단위테스트 추가

---
+++ user/src/test/java/com/example/user/MapControllerTest.java
@@ -0,0 +1,63 @@
+package com.example.user;
+
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONObject;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.user.dto.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MapControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    private String token;
+
+    private UserDto loginUser() {
+        String userId = "guest";
+        String password = "1234567";
+        UserDto userDto = new UserDto();
+        userDto.setUserId(userId);
+        userDto.setPassword(password);
+        return userDto;
+    }
+
+    @Test
+    public void 로그인() throws Exception {
+
+        UserDto user = loginUser();
+        String content = objectMapper.writeValueAsString(user);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                .content(content)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        byte[] response = mvcResult.getResponse().getContentAsByteArray();
+        JSONObject jsonResponse = new JSONObject(new String(response));
+        // 로그인 성공시 토큰을 담는다.
+        token = jsonResponse.get("accessToken").toString();
+        assertTrue(!"".equals(token));
+    }
+
+}
